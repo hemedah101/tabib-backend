b54b0eb6f391fad9b905962b5c29567e422d6cb9
Abdallah Hemedah <hemedah94@gmail.com>
2020-05-03 04:33:26 +0200

    setup ogma logger instead of nest logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TerminusModule } from '@nestjs/terminus';
+import { OgmaModule } from '@ogma/nestjs-module';
+import { ExpressParser } from '@ogma/platform-express';
 import { ApiConfigService } from './config/config.service';
 import expressConfig from './config/express.config';
 import { HealthController } from './health.controller';
@@ -8,6 +10,20 @@ import { HealthController } from './health.controller';
 @Module({
   imports: [
     ConfigModule.forRoot({ load: [expressConfig], isGlobal: true }),
+    OgmaModule.forRoot({
+      service: {
+        logLevel: 'ALL',
+        color: true,
+        json: false,
+        application: 'Tabib',
+      },
+      interceptor: {
+        http: ExpressParser,
+        ws: false,
+        gql: false,
+        rpc: false,
+      },
+    }),
     TerminusModule,
   ],
   controllers: [HealthController],
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { OgmaService } from '@ogma/nestjs-module';
 import { AppModule } from './app.module';
 import { ApiConfigService } from './config/config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { logger: false });
+  const logger = app.get<OgmaService>(OgmaService);
+  app.useLogger(logger);
+
   const config = app.get(ApiConfigService);
   await app.listen(config.port);
 
-  Logger.verbose(`http://localhost:${config.port}`);
+  logger.silly(`http://localhost:${config.port}`);
 }
 bootstrap();
